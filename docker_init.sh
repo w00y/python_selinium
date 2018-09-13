cd3ab813105aa9a30bbb4fc44c3b5e9b824d5cdd
w00y <42319314+w00y@users.noreply.github.com>
2018-09-13 23:48:30 +0100

    Update docker_init.sh

--- docker_init.sh
+++ docker_init.sh
@@ -1,7 +1,7 @@
 passwd root
 apt-get update
 dpkg --configure -a
-apt-get install nano unzip docker.io python-pip firefox xvfb  python3-pip -y
+apt-get install nano unzip docker.io python-pip firefox xvfb  python3-pip gedit -y
 pip3 install lxml PySocks stem
 wget https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodriver-v0.21.0-linux64.tar.gz 
 tar -xvf geckodriver-v0.21.0-linux64.tar.gz
