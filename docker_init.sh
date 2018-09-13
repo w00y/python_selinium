a46d5dc07ef1974d885ccb8c3667ee315084f9fe
w00y <42319314+w00y@users.noreply.github.com>
2018-09-14 00:32:23 +0100

    Update docker_init.sh

--- docker_init.sh
+++ docker_init.sh
@@ -10,3 +10,6 @@ chmod +x geckodriver
 cp geckodriver /usr/bin/geckodriver
 wget https://www.torproject.org/dist/torbrowser/8.0/tor-browser-linux64-8.0_en-US.tar.xz
 tar xvf tor-browser-linux64-8.0_en-US.tar.xz
+tor --hash-password 1234
+echo -e "ControlPort 9051\nHashedControlPassword 16:DDA28E1510D3786E60699CD89D361BF41DA855B5ADBC8F4D5DAFD0E8FE\nCookieAuthentication" >> /etc/tor/torrc
+service tor restart
